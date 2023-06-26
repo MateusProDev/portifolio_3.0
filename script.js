14f4a3bf2a1b9c67c2d2aa129237b858acef78fa
MateusProDev <130576866+MateusProDev@users.noreply.github.com>
2023-06-26 09:46:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const barbearia = document.querySelector('.containerNav');
+const navLinks = document.querySelector('.nav-links');
+
+barbearia.addEventListener('click', () => {
+  barbearia.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
 
