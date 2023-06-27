a8ce2ae32310aac5835d20d2a2c73cccb0a649cc
MateusProDev <130576866+MateusProDev@users.noreply.github.com>
2023-06-26 23:24:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const portifolio = document.querySelector('.containerNav');
+const barbearia = document.querySelector('.containerNav');
 const navLinks = document.querySelector('.nav-links');
 
-portifolio.addEventListener('click', () => {
-  portifolio.classList.toggle('active');
+barbearia.addEventListener('click', () => {
+  barbearia.classList.toggle('active');
   navLinks.classList.toggle('active');
 });
+
+// animação dos cards
+function activateBox(box) {
+  box.classList.toggle('active');
+}
+
+function rotateIn(box) {
+  if (!box.classList.contains('active')) {
+    box.classList.add('cardCodeBox-hover');
+  }
+}
+
+function rotateOut(box) {
+  box.classList.remove('cardCodeBox-hover');
+}
 
