37aa60dee4b5bfe53df964791d80e4e3247a9b29
MateusProDev <130576866+MateusProDev@users.noreply.github.com>
2023-06-28 21:50:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,43 @@ function rotateOut(box) {
   box.classList.remove('cardCodeBox-hover');
 }
 
+// mudadar o site de cor
+
+var body = document.body;
+var titulo = document.querySelector("h1");
+var paragrafos = document.querySelectorAll("p");
+var corOriginal = {
+  backgroundColor: body.style.backgroundColor,
+  color: body.style.color,
+  tituloColor: titulo.style.color,
+  paragrafoColors: []
+};
+
+paragrafos.forEach(function(paragrafo) {
+  corOriginal.paragrafoColors.push(paragrafo.style.color);
+});
+
+var elemento = document.getElementById("elemento");
+var clicado = false;
+
+elemento.addEventListener("click", function() {
+  if (clicado) {
+    body.style.backgroundColor = corOriginal.backgroundColor;
+    body.style.color = corOriginal.color;
+    titulo.style.color = corOriginal.tituloColor;
+    paragrafos.forEach(function(paragrafo, index) {
+    paragrafo.style.color = corOriginal.paragrafoColors[index];
+    });
+    clicado = false;
+  } else {
+    body.style.backgroundColor = "#11a1bb";
+    body.style.color = "black";
+    titulo.style.color = "#fff";
+    paragrafos.forEach(function(paragrafo) {
+      paragrafo.style.color = "";
+    });
+    clicado = true;
+  }
+});
+
+
